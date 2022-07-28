6f3f5f0abac5bfc8a29d7fbbb8957e2e4a51b4bd
Kevin Franco <kevifjq@gmail.com>
2022-07-28 17:34:53 -0300

    Do exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseString = "";
+    for(let i = string.length -1; i>=0;i--){
+        reverseString += string[i];
+    }
+    return reverseString;
 };
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
