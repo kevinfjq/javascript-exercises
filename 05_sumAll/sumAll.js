87bcf6d3565a16343e96a71139f4bdfdc61b9ea7
Kevin Franco <kevifjq@gmail.com>
2022-07-28 19:52:31 -0300

    Do exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if(typeof n1 !== "number" || typeof n2 !== "number" || n1<0 || n2<0){
+        return "ERROR";
+    }
+    let sum = 0;
+    let i = n1;
+    do{
+        sum += i;
+        if(n1>n2){
+            i--;
+        }
+        else{
+            i++
+        }
+    }while(i!=n2);
+    return sum + n2;
 };
 
 // Do not edit below this line
