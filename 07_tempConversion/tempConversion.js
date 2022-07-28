d98285209785d84108fc9515f2f520a871425cd8
Kevin Franco <kevifjq@gmail.com>
2022-07-28 20:20:14 -0300

    Do exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahr) {
+	cels = ((fahr - 32)*5) / 9;
+	cels = Math.round(cels * 10) / 10;
+	return cels;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+	fahr = ((cel * 9)+(5*32)) / 5;
+	fahr = Math.round(fahr * 10) / 10;
+	return fahr;
 };
 
 // Do not edit below this line
