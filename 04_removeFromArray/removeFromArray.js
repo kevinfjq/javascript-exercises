176ed5d63ea6b49105ef044141318c6a25291c39
Kevin Franco <kevifjq@gmail.com>
2022-07-28 19:27:20 -0300

    Do exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...num) {
+    let pos = 0;
+    let newArray;
+    for(let j=0; j<num.length;j++){
+        for(let i=0; i<array.length;i++){
+            if(array[i]=== num[j]){
+                pos = i;
+                newArray = array.splice(pos, 1);
+            }
+        }
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
